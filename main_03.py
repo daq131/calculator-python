70fae9ec8f5c07067c6e783171025ac892bc171c
Dawid Szarwark <daq131@gmail.com>
2025-01-09 23:04:15 -0400

    add logger

--- main_03.py
+++ main_03.py
@@ -1,32 +1,50 @@
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# def add(lista):
+#     logging.info(f"dodaję {len(lista)} elementy/ów")
+#     return sum(lista)
+
 def add(a, b):
-	return a + b
+    logging.info(f" Dodaję {a} i {b}")
+    return a + b
 
 def sub(a, b):
-	return a - b
+    logging.info(f" Odejmuję {a} i {b}")
+    return a - b
 
 def mul(a, b):
+    logging.info(f" Mnożę {a} i {b}")
     return a * b
 
 def div(a, b):
+    logging.info(f" Dzielę {a} i {b}")
     return a / b
 
 def get_data():
-	op = input("Operacja (+-*/): ")
-	a = input("a: ")
-	b = input("b: ")
+    op = input("Operacja (+-*/): ")
+    a = input("a: ")
+    b = input("b: ")
+    # n = int(input('podaj ile liczb chcesz uzyc do wykonania tego działania: '))
+    # lista = []
+    # for i in range(0, n):
+    #     lista.append(int(input('')))
 
-	return op, int(a), int(b)
+    return op, int(a), int(b)
+
 
 operations = {
-	"+": add,
-	"-": sub,
+    "+": add,
+    "-": sub,
     "*": mul,
     "/": div
 }
 
 def main():
-	op, a, b = get_data()
-	result = operations[op](a, b)
-	print("Wynik: ", result)
+    op, a, b = get_data()
+    # result = operations["+"](lista)
+    result = operations[op](a, b)
+    logging.info(f" Wynik to :{result}")
 
 main()
